d7dda04d66f085134fa5f7f0fc867670175da45d
PatrickDorneles <patrick.o.dorneles@gmail.com>
2021-09-30 19:13:08 -0300

    feat: changing favicon and cleaning project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app-container">
+    <app-title />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import AppTitle from "./components/AppTitle.vue";
 
-export default defineComponent({
+const App = defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    AppTitle,
   },
 });
+
+export default App;
 </script>
 
+<style lang="scss" scoped>
+.app-container {
+  height: 100vh;
+  width: 100vw;
+  background-color: #fff8e1;
+}
+</style>
+
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="title-container">
+    <fast-todo-title-logo class="title-logo" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import FastTodoTitleLogo from "./svg/FastTodoTitleLogo.vue";
+
+export default defineComponent({
+  components: {
+    FastTodoTitleLogo,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.title-container {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  top: 40px;
+  width: 100vw;
+  .title-logo {
+    transform: scale(4) translateX(-3px);
+  }
+}
+</style>
